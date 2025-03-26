4e482ba3c61301bef3e0bf3f48b491cd1b553fdd
unknown <you@example.com>
2025-03-26 16:47:09 -0300

    Update

--- Reports.R
+++ Reports.R
@@ -1,26 +1,40 @@
 #Load libraries
 library(readxl)
+library(readr)
 library(tidyverse)
 library(zoo)
 library(chron)
 library(lubridate)
 library(ggplot2)
+library(dplyr)
 
 #load data from Thailand World Mountain and Trail Running Championships 2021
-results2021 <- read_excel("data/02ResultsResults.xlsx", 
-                          col_types = c("text", "numeric", "text", "text", "text","date", "date","text", "text"))
+#results2021 <- read_excel("data/02ResultsResults.xlsx", 
+#                          col_types = c("text", "numeric", "text", "text", "text","date", "date","text", "text"))
+
+#write.csv(results2021, file = "data/WMTRC_Thailand.csv")
+
+#save as *.csv
+
+# Resultados WMTRC Thailand 2021 ####
+results2021 <- read_csv("data/WMTRC_Thailand.csv", col_types = cols(...1 = col_skip()))
 results2021$Date <- as.Date(results2021$Date)
 results2021$Time <- (times(format(results2021$Time, "%H:%M:%S")))
+results2021$Times <- paste(results2021$Date, results2021$Time )
+results2021$Times <- as.POSIXct(results2021$Times, format='%Y-%m-%d %H:%M:%OS')
+results2021 <- results2021 %>% select(-Date, -Time)
 
-#results2021$Times <- paste(results2021$Date, results2021$Time )
-#results2021$Times <- as.POSIXct(results2021$Times, format='%Y-%m-%d %H:%M:%OS')
-#results2021 <- results2021 %>% select(-Date, -Time)
+DNS <-  results2021 |> filter(Rank == "DNS")
+DNF <-  results2021 |> filter(Rank == "DNF")
+
+DNS
+DNF
 
 #Clean data
 results2021 <- na.omit(results2021)
 results2021$Rank <- as.numeric(results2021$Rank)
 
-#raw data
+#Subset data
 cl_2021 <- results2021 |> filter(Nation=="CHI")
 Long <- results2021 |> filter(Race =="Long Trail Race")  
 Short <- results2021 |> filter(Race =="Short Trail Race")  
@@ -30,37 +44,61 @@ Downhill <- results2021 |> filter(Race =="Up and Downhill Mountain Race")
 #Chilean results
 cl_2021 |>
   filter(Sexo == "Male") |>
-  select(Race, Rank, Bib, Name, Sexo, Time) |>
+  select(Race, Rank, Bib, Name, Sexo, Times) |>
   arrange(Race, Rank)
 
 cl_2021 |>
   filter(Sexo == "Female") |>
-  select(Race, Rank, Bib, Name, Sexo, Time) |>
+  select(Race, Rank, Bib, Name, Sexo, Times) |>
   arrange(Race, Rank)
 
-#General results
-boxplot(Long$Time~Long$Sexo, horizontal=T)
+# General results
+ggplot(Long, aes(x = Times, y = Sexo)) +
+  geom_boxplot() +
+  geom_jitter(aes(x = Times, y = Sexo), color = "red", size = 2, data = cl_2021 |> filter(Race == "Long Trail Race") ) +  # Puntos resaltados
+  scale_x_datetime(date_labels = "%H:%M:%S") +  # Formato eje X
+  labs(title = "Distribución de Tiempos por Sexo", x = "Tiempo (HH:MM:SS)", y = "Sexo") +
+  theme_minimal()
 summary(Long)
 results2021 |> filter(Race == "Long Trail Race" & Rank <=3 & Sexo == "Male")
 results2021 |> filter(Race == "Long Trail Race" & Rank <=3 & Sexo == "Female")
 
-boxplot(Short$Time~Short$Sexo, horizontal=T)
+ggplot(Short, aes(x = Times, y = Sexo)) +
+  geom_boxplot() +
+  geom_jitter(aes(x = Times, y = Sexo), color = "red", size = 2, data = cl_2021 |> filter(Race == "Short Trail Race") ) +  # Puntos resaltados
+  scale_x_datetime(date_labels = "%H:%M:%S") +  # Formato eje X
+  labs(title = "Distribución de Tiempos por Sexo", x = "Tiempo (HH:MM:SS)", y = "Sexo") +
+  theme_minimal()
 summary(Short)
 results2021 |> filter(Race == "Short Trail Race" & Rank <=3 & Sexo == "Male")
 results2021 |> filter(Race == "Short Trail Race" & Rank <=3 & Sexo == "Female")
 
-boxplot(Uphill$Time~Uphill$Sexo, horizontal=T)
+ggplot(Uphill, aes(x = Times, y = Sexo)) +
+  geom_boxplot() +
+  geom_jitter(aes(x = Times, y = Sexo), color = "red", size = 2, data = cl_2021 |> filter(Race == "Uphill Mountain Race") ) +  # Puntos resaltados
+  scale_x_datetime(date_labels = "%H:%M:%S") +  # Formato eje X
+  labs(title = "Distribución de Tiempos por Sexo", x = "Tiempo (HH:MM:SS)", y = "Sexo") +
+  theme_minimal()
 summary(Uphill)
 results2021 |> filter(Race == "Uphill Mountain Race" & Rank <=3 & Sexo == "Male")
 results2021 |> filter(Race == "Uphill Mountain Race" & Rank <=3 & Sexo == "Female")
 
-boxplot(Downhill$Time~Downhill$Sexo, horizontal=T)
+ggplot(Downhill, aes(x = Times, y = Sexo)) +
+  geom_boxplot() +
+  geom_jitter(aes(x = Times, y = Sexo), color = "red", size = 2, data = cl_2021 |> filter(Race == "Up and Downhill Mountain Race") ) +  # Puntos resaltados
+  scale_x_datetime(date_labels = "%H:%M:%S") +  # Formato eje X
+  labs(title = "Distribución de Tiempos por Sexo", x = "Tiempo (HH:MM:SS)", y = "Sexo") +
+  theme_minimal()
 summary(Downhill)
 results2021 |> filter(Race == "Up and Downhill Mountain Race" & Rank <=3 & Sexo == "Male")
 results2021 |> filter(Race == "Up and Downhill Mountain Race" & Rank <=3 & Sexo == "Female")
 
 #Medals
-medals <- results2021 |> mutate(Medal = car::recode(results2021$Rank, '1="Gold"; 2="Silver"; 3="Bronze"; else = NA'))
+medals <- results2021 |> mutate(Medal = case_when(Rank == 1 ~ "Gold",
+                                                  Rank == 2 ~ "Silver",
+                                                  Rank == 3 ~ "Bronze",
+                                                  TRUE ~ NA_character_))
+
 medals <- na.omit(medals) 
 
 # Contar medallas Generales por país
